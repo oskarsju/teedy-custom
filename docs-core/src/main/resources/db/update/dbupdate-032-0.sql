d8a5b352250305526c0b0e41f760923ad1558112
oskarsju <77115013+oskarsju@users.noreply.github.com>
2026-08-17 11:25:55 +0300

    Make long comment migration database-specific

--- docs-core/src/main/resources/db/update/dbupdate-032-0.sql
+++ docs-core/src/main/resources/db/update/dbupdate-032-0.sql
@@ -1,7 +1,7 @@
 -- DBUPDATE-032-0.SQL
 
--- Allow long comment/note content
-alter table T_COMMENT alter column COM_CONTENT_C type text;
+!PGSQL!alter table T_COMMENT alter column COM_CONTENT_C type text;
+!H2!alter table T_COMMENT alter column COM_CONTENT_C longvarchar;
 
 -- Update the database version
 update T_CONFIG set CFG_VALUE_C = '32' where CFG_ID_C = 'DB_VERSION';
